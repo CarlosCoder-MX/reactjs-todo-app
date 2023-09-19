a6dabce57514dbed485eaa0c2e284fdc87f2377f
CarlosCoder-MX <carloscoder.contact@gmail.com>
2023-09-19 11:39:56 -0600

    Added styles and animations

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,9 +2,9 @@ import Checkbox from "./Checkbox";
 
 export default function Task({name,done,onToggle}) {
   return (
-    <div className="task">
-        <Checkbox Checked={done} onClick={() => onToggle(!done)} />
-        {name}
+    <div className={'task ' + (done?'done':'')}>
+        <Checkbox checked={done} onClick={() => onToggle(!done)} />
+        <span>{name}</span>
     </div>
   );
 }
